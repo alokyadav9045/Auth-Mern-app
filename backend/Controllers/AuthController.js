0b42189cb0a7ebb00cc91c3c86b9ebe2fd9dfe62
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2025-01-09 15:47:41 +0530

    Update AuthController.js

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../Models/User');
+const UserModel = require('../Models/User');
 
 const signup = async (req, res) => {
     try {
