022fb6ceb854de3fa522f3d7f6463da1ab3d6553
Alok Yadav <163675739+alokyadav9045@users.noreply.github.com>
2025-01-09 16:45:01 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = "https://auth-mern-app-iu0asgv7j-alokyadav9045s-projects.vercel.app/auth/login";
+            const url = "https://auth-mern-app-api-eosin.vercel.app/auth/login";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
